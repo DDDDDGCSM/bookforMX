b4387f7b911e31d65a2f2eab09e76959b0f16a7b
58 <a58@58deMacBook-Pro.local>
2026-01-08 21:45:20 +0800

    Clean: Remove non-project files - 2026-01-08 21:45:20

--- quick_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-python3 -m pip install --trusted-host pypi.org --trusted-host pypi.python.org --trusted-host files.pythonhosted.org Flask==3.0.0 Flask-CORS==4.0.0 && python3 app.py
